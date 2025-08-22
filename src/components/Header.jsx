31d830804d0ace18b311646636909a1f8f76c649
Yavuz Selim Çakmak <cakmakselimfb58@gmail.com>
2025-08-22 23:11:33 +0300

    black navbar with explanation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className='header'>
+    <div className="header">
       <Link className='link' to="/">Anasayfa</Link>
       <Link className='link' to="/about">Hakkımızda</Link>
       <Link className='link' to="/products">Ürünler</Link>
       <Link className='link' to="/contacts">İletişim</Link>
-      <Link className="link" to="/fault">Arıza Talep Formu</Link>
+
+      <div className="link-with-subtext">
+        <Link className="link" to="/fault">Arıza Talep Formu</Link>
+        <span className="subtext">Formu doldur, seni hemen arayalım</span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
